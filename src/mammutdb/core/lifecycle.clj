978d90550dc60661e35a3f8f7d063e7101e72e77
Andrey Antukh <niwi@niwi.be>
2014-06-09 22:24:16 +0200

    Add joinable lifecycle variant.

--- src/mammutdb/core/lifecycle.clj
+++ src/mammutdb/core/lifecycle.clj
@@ -4,3 +4,6 @@
 (defprotocol Lifecycle
   (start [_] "Initialize/Start process.")
   (stop [_] "Die/Stop process."))
+
+(defprotocol Joinable
+  (join [_] "Join to current proces until it finishes."))
