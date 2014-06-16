5460c89f0e48548bb662434bbb3d491b7b5209e0
Andrey Antukh <niwi@niwi.be>
2014-06-16 23:19:13 +0200

    Add initial cli module with migration bootstraping.

--- src/mammutdb/cli.clj
+++ src/mammutdb/cli.clj
@@ -2,10 +2,12 @@
   "Main entry point for command line interface
   of mammutdb."
   (:require [mammutdb.config :as conf]
-            [mammutdb.storage.migrations :as migrations])
+            [mammutdb.storage.migrations :as migrations]
+            [cats.types :as t]
+            [cats.core :as m])
   (:gen-class))
 
 (defn -main
   [configpath]
   (alter-var-root #'conf/*config-path* (fn [_] configpath))
-  (migrations/initialize!))
+  (migrations/bootstrap))
