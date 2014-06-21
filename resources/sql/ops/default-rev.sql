014e82f73c00a13e09314f9d7d101b6e6791358d
Andrey Antukh <niwi@niwi.be>
2014-06-21 13:38:08 +0200

    Rename directory coll-schema to ops.

--- resources/sql/ops/default-rev.sql
+++ resources/sql/ops/default-rev.sql
@@ -1,8 +1,8 @@
 CREATE TABLE %s (
        id uuid DEFAULT uuid_generate_v1() UNIQUE PRIMARY KEY,
        data json DEFAULT '{}'::json,
-       revision bigint DEFAULT 1,
+       revision json DEFAULT uuid_generate_v1(),
        created_at timestamp with time zone DEFAULT now(),
-       updated_at timestamp with time zone DEFAULT now()
 );
+
 
