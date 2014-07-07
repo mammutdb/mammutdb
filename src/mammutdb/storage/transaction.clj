6b5eac7acf45aa496574ba288adeb25f83f5eb4f
Andrey Antukh <niwi@niwi.be>
2014-07-07 23:30:32 +0200

    Add transaction abstraction removing repeated code from high level api.

--- src/mammutdb/storage/transaction.clj
+++ src/mammutdb/storage/transaction.clj
@@ -1,12 +1,22 @@
 (ns mammutdb.storage.transaction
   "Transaction abstractions."
   (:require [cats.core :as m]
-            [cats.types :as t]))
+            [cats.types :as t]
+            [mammutdb.storage.connection :as sconn]))
+
 
 (defn run-in-transaction
   [conn func & [{:keys [retries readonly] :or {retries 3 readonly false}}]]
   (let [conn (t/just conn)]
     (m/>>= conn func)))
+
+(defn transaction
+  ([func] (transaction {} func))
+  ([options func]
+     (m/mlet [conn   (sconn/new-connection)
+              result (run-in-transaction conn func options)
+              _      (sconn/close-connection conn)]
+       (m/return result))))
 
 ;; (loop [retry 0]
 ;;   (try
