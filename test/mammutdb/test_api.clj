4917a9ecd4bf703a0fee0cd52b38eae19d7fb5ad
Andrey Antukh <niwi@niwi.be>
2014-07-12 14:28:40 +0200

    Fixing tests.

--- test/mammutdb/test_api.clj
+++ test/mammutdb/test_api.clj
@@ -2,8 +2,6 @@
   (:require [clojure.test :refer :all]
             [cats.types :as t]
             [mammutdb.api :as api]
-            [mammutdb.api.database :as dbapi]
-            [mammutdb.api.collection :as collapi]
             [mammutdb.storage.database :as sdb]
             [mammutdb.storage.collection :as scoll]
             [mammutdb.config :as config]
@@ -14,8 +12,8 @@
   (migrations/bootstrap)
 
   (testing "Common crud functions"
-    (let [mv1 (api/create-db "foodb1")
-          mv2 (api/create-db "foodb2")]
+    (let [mv1 (api/create-database "foodb1")
+          mv2 (api/create-database "foodb2")]
       (is (t/right? mv1))
       (is (t/right? mv2)))
 
@@ -25,8 +23,8 @@
       (is (vector? v))
       (is (= (count v) 2)))
 
-    (api/drop-db "foodb1")
-    (api/drop-db "foodb2")
+    (api/drop-database "foodb1")
+    (api/drop-database "foodb2")
 
     (let [mv (api/get-all-databases)
           v  (t/from-either mv)]
@@ -35,13 +33,13 @@
       (is (= (count v) 0))))
 
   (testing "Get by name"
-    (let [mv1 (api/create-db "foodb1")
-          mv2 (api/create-db "foodb2")]
+    (let [mv1 (api/create-database "foodb1")
+          mv2 (api/create-database "foodb2")]
       (is (t/right? mv1))
       (is (t/right? mv2)))
 
-    (let [mv1 (api/get-db-by-name "foodb1")
-          mv2 (api/get-db-by-name "foodb2")
+    (let [mv1 (api/get-database-by-name "foodb1")
+          mv2 (api/get-database-by-name "foodb2")
           v1  (t/from-either mv1)
           v2  (t/from-either mv2)]
       (is (t/right? mv1))
@@ -49,15 +47,15 @@
       (is (sdb/database? v1))
       (is (sdb/database? v2)))
 
-    (api/drop-db "foodb1")
-    (api/drop-db "foodb2")))
+    (api/drop-database "foodb1")
+    (api/drop-database "foodb2")))
 
 (deftest collection-api
   (config/setup-config! "test/testconfig.edn")
   (migrations/bootstrap)
 
   (testing "Common crud functions"
-    (let [mdb (api/create-db "foodb")
+    (let [mdb (api/create-database "foodb")
           mv1 (api/create-collection "foodb" "collname1")
           mv2 (api/create-collection "foodb" "collname2")
           v1  (t/from-either mv1)
@@ -83,4 +81,4 @@
       (is (vector? r))
       (is (= (count r) 0)))
 
-    (api/drop-db "foodb")))
+    (api/drop-database "foodb")))
